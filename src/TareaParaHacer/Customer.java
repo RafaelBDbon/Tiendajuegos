b67f96ca8fea9dbf5b97093c73b87c26ee829be6
RafaelBDbon <rafael.barahona@educa.madrid.org>
2026-09-14 17:16:18 +0200

    Clases creadas

--- src/TareaParaHacer/Customer.java
+++ src/TareaParaHacer/Customer.java
@@ -3,7 +3,7 @@ package TareaParaHacer;
 public class Customer {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
--- src/TareaParaHacer/GENRE.java
+++ src/TareaParaHacer/GENRE.java
@@ -2,4 +2,5 @@ package TareaParaHacer;
 
 public enum GENRE {
 
+
 }
